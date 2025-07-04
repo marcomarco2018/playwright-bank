384c74171bf32a6df594280dfb0973eef9001ca1
Smart Tech <smartechbeat@gmail.com>
2025-07-03 19:24:12 -0600

    Updated sendTransferModalPage

--- pages/sendTransferModal.ts
+++ pages/sendTransferModal.ts
@@ -7,6 +7,7 @@ export class SendTransferModal {
     readonly receiverEmailInput: Locator;
     readonly sourceAccountComboBox: Locator;
     readonly amountToTransferInput: Locator;
+    readonly CancelButton: Locator;
     readonly sendButton: Locator;
 
 
@@ -16,36 +17,9 @@ export class SendTransferModal {
         this.receiverEmailInput = page.getByRole('textbox', {name: 'Email del destinatario *'});
         this.sourceAccountComboBox = page.getByRole('combobox', { name: 'Cuenta origen *' });
         this.amountToTransferInput = page.getByRole('spinbutton', { name: 'Monto a enviar *' });
-        this.sendButton = page.getByTestId('boton-enviar');
+        this.CancelButton = page.getByRole('button', { name: 'Cancelar' });
+        this.sendButton = page.getByRole('button', { name: 'Enviar' });
     }
 
 
-    async selectAccountType(accountType: string){
-        await this.AccountTypeComboBox.click();
-
-        try {                 
-
-            await this.page.getByRole('option', {name: accountType}).click();
-            
-        } catch (error) {
-            console.log(`element not found: ${error}`)
-        }
-    
-    }
-
-    async enterAmount(amount: string){
-        await this.InitialAmountInput.fill(amount);
-    }
-
-    async clickCreateAccountButton(){
-        await this.CreateAccountButton.click();
-    }
-
-    async clickCancelAccountCreationButton(){
-        await this.CancelAccountCreationButton.click();
-    }
-
-
-
-
-} 
+}
